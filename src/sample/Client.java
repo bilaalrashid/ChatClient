0fc30a83ea7c8b5407c8452d19e4c7db5ed01fd3
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-26 22:40:39 +0100

    Added client controller for sending and receiving data from a server

---
+++ src/sample/Client.java
@@ -0,0 +1,79 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * The client
+ */
+class Client {
+
+    // Properties
+
+    /**
+     * The input channel of the client
+     */
+    private BufferedReader clientInput;
+
+    /**
+     * The output channel of the client
+     */
+    private PrintWriter clientOutput;
+
+    /**
+     * The current line of input from the server
+     */
+    private String currentInput = null;
+
+    // Constructor
+
+    /**
+     * Creates a new client
+     * @param host The domain that the server is located at
+     * @param port The port the server is listening on
+     * @throws IOException Error setting up socket stream
+     */
+    Client(String host, int port) throws IOException {
+        Socket kkSocket = new Socket(host, port);
+        this.clientInput = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+        this.clientOutput = new PrintWriter(kkSocket.getOutputStream(), true);
+    }
+
+    // Send
+
+    /**
+     * Sends a message to the server
+     * @param message The message to send
+     */
+    void sendMessage(String message) {
+        this.clientOutput.println(message);
+    }
+
+    // Receive
+
+    /**
+     * Is the server currently sending data to the client
+     * @return If the server is currently sending data
+     * @throws IOException Error receiving input
+     */
+    boolean isReceiving() throws IOException {
+        return (this.currentInput = this.clientInput.readLine()) != null;
+    }
+
+    /**
+     * Gets the message being send to the client
+     * @return The received message
+     * @throws IOException Error receiving input
+     */
+    String getMessage() throws IOException {
+        if (this.currentInput != null) {
+            return this.currentInput;
+        }
+
+        throw new IOException("No input received");
+    }
+
+}
