79864b4524bd46479492198c780ae7a5cd0b962c
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-26 21:19:44 +0100

    Added port number to command line arguments

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The main interface for the client
+ */
 public class Main extends Application {
 
+    /**
+     * Displays the window for the UI
+     * @param primaryStage The main window to show
+     * @throws Exception The FXML can't be found
+     */
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Chat Client");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
+    /**
+     * Gets the port to run the client on and starts the UI
+     * @param args The command lin arguments
+     */
     public static void main(String[] args) {
-        launch(args);
+        if (args != null && args.length == 1) {
+            int port = Integer.parseInt(args[0]);
+            launch(args);
+        } else {
+            System.out.println("Invalid arguments. Port not specified.");
+        }
     }
+
 }
