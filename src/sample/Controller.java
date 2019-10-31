e05f5b34a841925cfd8ad38efda2e0fa06ab34c3
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-31 17:15:49 +0000

    Added GUI for sending message

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Controller for main stage
+ */
 public class Controller {
+
+    /**
+     * The TextField to input a new message
+     */
+    @FXML
+    private TextField messageInput;
+
+    /**
+     * Sends the inputted message
+     */
+    @FXML
+    private void sendMessage() {
+        String message = this.messageInput.getText();
+    }
+
 }
